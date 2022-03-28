5524fe4a28b3bab4ddc36dad53db02e4bb1fd994
avinh <avinhahuynh@gmail.com>
2022-03-28 08:58:39 -0700

    palindrome

--- week 2/palindromeClass.py
+++ week 2/palindromeClass.py
@@ -1,21 +1,34 @@
-import re
+import math
 
-class palindrome:
+class Palindrome:
     def __init__(self):
         self.index = 0
         self.input = ""
-        self.cleanList = ["\s", "-", ",", "!", "?", "."]
+        self.cleanList = [" ", "-", ",", "!", "?", "."]
 
     def clean(self):
         self.input = self.input.lower()
         for element in self.cleanList:
-            self.input.replace(element, '')
+            self.input = self.input.replace(element, '')
+        return self.input
 
     def __call__(self, input):
         self.input = input
         self.clean()
-        print(self.input)
+
+        index = math.floor(len(self.input)/2)
+        palindrome = True
+
+        for i in range(index):
+            if self.input[i] != self.input[::-1][i]:
+                print("Error on index: " + str(i), str((len(self.input) - i)))
+                palindrome = False
+
+        if palindrome == False:
+            print("This is not a palindrome")
+        else:
+            print("This is a palindrome")
 
 
-pal = palindrome()
-pal("giraf-fe")
+pal = Palindrome()
+pal("tvfcdrtvfybghujimkol.p")
