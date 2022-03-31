a3057535c4402c3b48a1a84b2b070387fdcb7334
Naevey <fpsgaaa@gmail.com>
2022-03-31 11:04:47 -0700

    scraping works

--- week 3/webscraper.py
+++ week 3/webscraper.py
@@ -4,5 +4,10 @@ from bs4 import BeautifulSoup
 URL = "https://www.x-rates.com/table/?from=USD&amount=1"
 r = requests.get(URL) # this opens the website
 
-soup = BeautifulSoup(r.content, 'html.parser')
-print(soup)
+soup = BeautifulSoup(r.content, 'html.parser') # prints the website content
+ratelist = soup.findAll("table", {"class": "ratesTable"})[0].findAll("tbody") # this finds the table
+
+for i in ratelist:
+    trList = i.findAll('tr') # tr is the thing before the text in the html file, .findall finds all instances
+    for j in trList[:10]: # prints the 10 table contents
+        print(j.text)
